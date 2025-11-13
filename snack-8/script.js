8419c9ca14902d656a8d73ea03609369b429374f
ilariamotta <ilaria.motta93@gmail.com>
2025-11-13 16:12:22 +0100

    feat: snack 8 completato

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,14 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+
+sezioneStudente = students.find(function (studente, index) {
+  if (studente.name === "Marco Lanci") {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+console.log(sezioneStudente.class);
