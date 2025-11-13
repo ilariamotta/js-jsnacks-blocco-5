f752e8d2deb90bb9cde6a685c71961e830cf4c1d
ilariamotta <ilaria.motta93@gmail.com>
2025-11-13 15:48:16 +0100

    feat: snack 5 completato

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,13 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const arrayNumPari = nums.filter(function (number) {
+    if (number % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+});
+
+console.log(arrayNumPari);
