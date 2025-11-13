dd0d3abdafedb9dd02f6a95dcc90af0b2ec63ed6
ilariamotta <ilaria.motta93@gmail.com>
2025-11-13 16:03:12 +0100

    feat: snack 7 completato

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,14 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+
+student = students.find (function(name, index){
+  if (name.id === 2) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+console.log(student);
